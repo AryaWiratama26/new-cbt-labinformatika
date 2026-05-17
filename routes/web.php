382752f0aaab5afe2d8e7de1e2e2eb291fd15b9d
Arya Wiratama <aryawiratama2401@gmail.com>
2026-05-17 11:41:08 +0700

    fix: increase login throttle from 5 to 100

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:100,1');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
